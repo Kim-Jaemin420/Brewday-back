cc3d48c04f6733f55d4dd80b5bb9a5fd0bb89827
Kim-Jaemin420 <jany240@naver.com>
2021-06-02 22:43:30 +0900

    feat: 포스트 수정

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -59,3 +59,45 @@ exports.remove = (ctx) => {
   posts.splice(index, 1);
   ctx.status = 204;
 };
+
+// 포스트 수정
+// PUT /api/posts/:id
+exports.replace = (ctx) => {
+  const { id } = ctx.params;
+
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+// 포스트 수정(특정 필드 변경)
+exports.update = (ctx) => {
+  const { id } = ctx.params;
+
+  const index = posts.findIndex((p) => p.id.toString() === id);
+
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
